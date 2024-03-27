467066349a0e2608890c61f65e26e7ec7ad7885e
Daniel Vaughan <1843788+DVaughan@users.noreply.github.com>
2024-03-27 22:26:12 +0100

    Re-implementation of ReadOnlyAdaptiveCollection.

--- Source/Framework/Calcium.Extras/Collections/AdaptiveCollections/ReadOnlyAdaptiveCollection.cs
+++ Source/Framework/Calcium.Extras/Collections/AdaptiveCollections/ReadOnlyAdaptiveCollection.cs
@@ -12,315 +12,205 @@
 */
 #endregion
 
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
-using Calcium.ComponentModel;
+using System;
+using System.Collections;
 
 namespace Calcium.Collections
 {
 	/// <summary>
-	/// An adaptive collection allows you to combine two different collection types.
-	/// It allows you to materialize the objects in the inner collection,
-	/// which are of type <c>TInner</c> as objects of type <c>T</c>
-	/// using a <see cref="IAttachObject{T}"/> implementation.
-	/// This class is useful, for example, in materializing a collection 
-	/// of <c>ICommand</c> objects as a set of <c>DropDownMenuItem</c> objects.
-	/// This class is a read-only variant of the <see cref="AdaptiveCollection{T,TInner}"/>.
+	/// A read-only collection that adapts items from one type to another using a provided factory function.
+	/// It listens to changes from an underlying ReadOnlyObservableCollection and updates accordingly.
 	/// </summary>
-	/// <typeparam name="T">
-	/// The type of objects that can be bound to a list.
-	/// </typeparam>
-	/// <typeparam name="TInner">
-	/// The type of objects contained in the source collection.</typeparam>
-	public class ReadOnlyAdaptiveCollection<T, TInner> : IReadOnlyList<T>,
-														INotifyCollectionChanged, 
-														INotifyPropertyChanged,
-														IList<T>,
-														IDisposable
-		where T : class, IAttachObject<TInner>, new()
+	/// <typeparam name="TTo">The type of objects that the collection will contain.</typeparam>
+	/// <typeparam name="TFrom">The type of objects contained in the source collection.</typeparam>
+	public class ReadOnlyAdaptiveCollection<TTo, TFrom> : IEnumerable<TTo>,
+														  IReadOnlyList<TTo>,
+														  INotifyCollectionChanged,
+														  INotifyPropertyChanged,
+														  IDisposable where TFrom : notnull
 	{
-	
-		readonly Dictionary<TInner, T> lookupDictionary
-			= new Dictionary<TInner, T>();
-	
-		readonly List<T> items = new List<T>();
-	
-		readonly ReadOnlyObservableCollection<TInner> monitoredCollection;
-		public ReadOnlyObservableCollection<TInner> MonitoredCollection => monitoredCollection;
-	
-		bool ignoreChanges;
-	
-		public void Dispose()
+		readonly ReadOnlyObservableCollection<TFrom> monitoredCollection;
+		readonly Dictionary<TFrom, TTo> itemsMap = new();
+		readonly ObservableCollection<TTo> items = new();
+
+		public ReadOnlyObservableCollection<TTo> ReadOnlyObservableCollection { get; }
+
+		readonly Func<TFrom, TTo> getItem;
+		bool handlingChange;
+
+		public ReadOnlyAdaptiveCollection(
+			ReadOnlyObservableCollection<TFrom> monitoredCollection,
+			Func<TFrom, TTo> getItem)
 		{
-			((INotifyCollectionChanged)monitoredCollection).CollectionChanged -= HandleCollectionChanged;
-			((INotifyPropertyChanged)monitoredCollection).PropertyChanged -= HandlePropertyChanged;
-			lookupDictionary.Clear();
-			items.Clear();
+			this.monitoredCollection = monitoredCollection ?? throw new ArgumentNullException(nameof(monitoredCollection));
+			this.getItem = getItem ?? throw new ArgumentNullException(nameof(getItem));
+
+			AdaptItems();
+
+			ReadOnlyObservableCollection = new(items);
+
+			((INotifyCollectionChanged)monitoredCollection).CollectionChanged += OnMonitoredCollectionChanged;
 		}
-	
-		public ReadOnlyAdaptiveCollection(ReadOnlyObservableCollection<TInner> monitoredCollection)
+
+		void OnMonitoredCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
 		{
-			this.monitoredCollection = monitoredCollection 
-										?? throw new ArgumentNullException(nameof(monitoredCollection));
-	
-			((INotifyCollectionChanged)this.monitoredCollection).CollectionChanged += HandleCollectionChanged;
-			((INotifyPropertyChanged)this.monitoredCollection).PropertyChanged += HandlePropertyChanged;
-
-			SynchronizeCollections();
-		}
-	
-		void HandlePropertyChanged(object sender, PropertyChangedEventArgs e)
-		{
-			PropertyChanged?.Invoke(this, e);
-		}
-	
-		void HandleCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
-		{
-			if (ignoreChanges)
+			if (handlingChange)
 			{
-				return;
+				throw new InvalidOperationException("Reentrant modification detected on the monitored collection.");
 			}
-	
-			NotifyCollectionChangedAction action = e.Action;
-	
-			if (action == NotifyCollectionChangedAction.Add)
+
+			try
 			{
-				var list = new List<T>();
-	
-				foreach (TInner command in e.NewItems)
-				{
-					var attacher = new T();
-					attacher.AttachObject(command);
-					lookupDictionary[command] = attacher;
-					list.Add(attacher);
-				}
-
-				items.AddRange(list);
-
-				OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, list));
-				RaisePropertyCountChanged();
+				handlingChange = true;
+				ProcessChangeEvent(e);
 			}
-			else if (action == NotifyCollectionChangedAction.Move)
+			finally
 			{
-				int movedFromIndex = e.OldStartingIndex;
-				int movedToIndex = e.NewStartingIndex;
-				int itemCount = e.OldItems?.Count ?? 0;
-				T movingItem = items[movedFromIndex];
-				items.RemoveAt(movedFromIndex);
-				items.Insert(movedToIndex, movingItem);
-				
-				OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Move, movingItem, movedFromIndex, movedToIndex));
-			}
-			else if (action == NotifyCollectionChangedAction.Remove)
-			{
-				var oldItems = e.OldItems;
-	
-				var itemsToRemove = new List<T>();
-	
-				foreach (TInner oldItem in oldItems)
-				{
-					if (lookupDictionary.TryGetValue(oldItem, out T owner) && owner != null)
-					{
-						itemsToRemove.Add(owner);
-					}
-	
-					lookupDictionary.Remove(oldItem);
-				}
-				
-				Exception disposeException = null;
-
-				foreach (T owner in itemsToRemove)
-				{
-					items.Remove(owner);
-					
-					try
-					{
-						owner.DetachObject();
-
-						var disposable = owner as IDisposable;
-						disposable?.Dispose();
-					}
-					catch (Exception ex)
-					{
-						disposeException = ex;
-					}
-				}
-
-				if (disposeException != null)
-				{
-					throw new Exception("Problem detaching owner.", disposeException);
-				}
-
-				OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, itemsToRemove));
-				RaisePropertyCountChanged();
-			}
-			else if (action == NotifyCollectionChangedAction.Replace)
-			{
-				var removedItems = e.OldItems;
-	
-				var itemsToRemove = new List<T>();
-	
-				foreach (TInner oldItem in removedItems)
-				{
-					if (lookupDictionary.TryGetValue(oldItem, out T owner) && owner != null)
-					{
-						itemsToRemove.Add(owner);
-					}
-	
-					lookupDictionary.Remove(oldItem);
-				}
-	
-				foreach (T owner in itemsToRemove)
-				{
-					try
-					{
-						ignoreChanges = true;
-						items.Remove(owner);
-					}
-					finally
-					{
-						ignoreChanges = false;
-					}
-	
-					owner.DetachObject();
-	
-					var disposable = owner as IDisposable;
-					disposable?.Dispose();
-				}
-	
-				var newItems = e.NewItems;
-				var replacements = new List<T>();
-
-				foreach (TInner newItem in newItems)
-				{
-					var index = monitoredCollection.IndexOf(newItem);
-	
-					var attacher = new T();
-					attacher.AttachObject(newItem);
-					lookupDictionary[newItem] = attacher;
-					replacements.Add(attacher);
-
-					try
-					{
-						ignoreChanges = true;
-						itemsToRemove.Insert(index, attacher);
-					}
-					finally
-					{
-						ignoreChanges = false;
-					}
-				}
-
-				items.ReplaceRange(itemsToRemove, replacements);
-
-				OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, replacements, itemsToRemove));
-			}
-			else if (action == NotifyCollectionChangedAction.Reset)
-			{
-				var list = new List<T>(items);
-				items.Clear();
-	
-				foreach (var owner in list)
-				{
-					if (owner == null)
-					{
-						continue;
-					}
-	
-					owner.DetachObject();
-	
-					var disposable = owner as IDisposable;
-					disposable?.Dispose();
-				}
-
-				SynchronizeCollections();
-
-				OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
-				RaisePropertyCountChanged();
+				handlingChange = false;
 			}
 		}
 
-		void SynchronizeCollections()
+		void ProcessChangeEvent(NotifyCollectionChangedEventArgs e)
 		{
-			lookupDictionary.Clear();
-			items.Clear();
+			bool countChanged = false;
 
-			foreach (TInner item in monitoredCollection)
+			switch (e.Action)
 			{
-				T attacher = new T();
-				attacher.AttachObject(item);
-				lookupDictionary[item] = attacher;
+				case NotifyCollectionChangedAction.Add:
+					{
+						if (e.NewItems != null)
+						{
+							foreach (TFrom newItem in e.NewItems)
+							{
+								TTo adaptedItem = getItem(newItem);
+								itemsMap.Add(newItem, adaptedItem);
+								items.Add(adaptedItem);
+							}
 
-				items.Add(attacher);
+							countChanged = true;
+							CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, e.NewItems));
+						}
+						break;
+					}
+
+				case NotifyCollectionChangedAction.Remove:
+					{
+						if (e.OldItems != null)
+						{
+							foreach (TFrom oldItem in e.OldItems)
+							{
+								if (itemsMap.TryGetValue(oldItem, out TTo? itemToRemove))
+								{
+									itemsMap.Remove(oldItem);
+									items.Remove(itemToRemove);
+									CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, itemToRemove));
+								}
+							}
+
+							countChanged = true;
+						}
+
+						break;
+					}
+
+				case NotifyCollectionChangedAction.Replace:
+					{
+						if (e is { OldItems: not null, NewItems: not null } && e.OldItems.Count == e.NewItems.Count)
+						{
+							for (int i = 0; i < e.OldItems.Count; i++)
+							{
+								if (e.OldItems[i] is TFrom oldItem && e.NewItems[i] is TFrom newItem)
+								{
+									var newAdaptedItem = getItem(newItem);
+
+									if (itemsMap.TryGetValue(oldItem, out TTo? oldAdaptedItem))
+									{
+										itemsMap.Remove(oldItem);
+										itemsMap.Add(newItem, newAdaptedItem);
+
+										int index = items.IndexOf(oldAdaptedItem);
+										if (index != -1)
+										{
+											items[index] = newAdaptedItem;
+										}
+										CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, newAdaptedItem, oldAdaptedItem, index));
+									}
+								}
+							}
+						}
+						break;
+					}
+
+				case NotifyCollectionChangedAction.Move:
+					{
+						if (e is { OldStartingIndex: >= 0, NewStartingIndex: >= 0, OldItems: not null })
+						{
+							foreach (TFrom item in e.OldItems)
+							{
+								TTo movingItem = itemsMap[item];
+								items.Remove(movingItem);
+								items.Insert(e.NewStartingIndex, movingItem);
+							}
+
+							CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Move, e.OldItems, e.NewStartingIndex, e.OldStartingIndex));
+						}
+						break;
+					}
+
+				case NotifyCollectionChangedAction.Reset:
+					{
+						itemsMap.Clear();
+						items.Clear();
+
+						AdaptItems();
+
+						CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+
+						countChanged = true;
+
+						break;
+					}
+			}
+
+			if (countChanged)
+			{
+				OnPropertyChanged(new PropertyChangedEventArgs(nameof(Count)));
 			}
 		}
 
-		void RaisePropertyCountChanged()
+		void AdaptItems()
 		{
-			OnPropertyChanged(new PropertyChangedEventArgs(nameof(Count)));
-			OnPropertyChanged(new PropertyChangedEventArgs("Item[]"));
+			foreach (var item in monitoredCollection)
+			{
+				TTo adaptedItem = getItem(item);
+				itemsMap.Add(item, adaptedItem);
+				items.Add(adaptedItem);
+			}
 		}
-		
-		public event NotifyCollectionChangedEventHandler CollectionChanged;
 
-		protected virtual void OnCollectionChanged(NotifyCollectionChangedEventArgs e) 
-			=> CollectionChanged?.Invoke(this, e);
+		public IEnumerator<TTo> GetEnumerator() => items.GetEnumerator();
 
-		public event PropertyChangedEventHandler PropertyChanged;
-
-		protected virtual void OnPropertyChanged(PropertyChangedEventArgs e) 
-			=> PropertyChanged?.Invoke(this, e);
-
-		public IEnumerator<T> GetEnumerator() => items.GetEnumerator();
-	
 		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
-		/// <inheritdoc />
-		public void Add(T item) => throw new NotSupportedException();
+		public event NotifyCollectionChangedEventHandler? CollectionChanged;
 
-		/// <inheritdoc />
-		public void Clear() => throw new NotSupportedException();
+		public event PropertyChangedEventHandler? PropertyChanged;
 
-		/// <inheritdoc />
-		public bool Contains(T item) => items.Contains(item);
+		protected virtual void OnPropertyChanged(PropertyChangedEventArgs e) => PropertyChanged?.Invoke(this, e);
 
-		/// <inheritdoc />
-		public void CopyTo(T[] array, int arrayIndex) => items.CopyTo(array, arrayIndex);
-
-		/// <inheritdoc />
-		public bool Remove(T item) => throw new NotSupportedException();
+		public void Dispose()
+		{
+			((INotifyCollectionChanged)monitoredCollection).CollectionChanged -= OnMonitoredCollectionChanged;
+		}
 
 		public int Count => items.Count;
 
-		/// <inheritdoc />
 		public bool IsReadOnly => true;
 
-		/// <inheritdoc />
-		public int IndexOf(T item) => items.IndexOf(item);
-
-		/// <inheritdoc />
-		public void Insert(int index, T item) => throw new NotSupportedException();
-
-		/// <inheritdoc />
-		public void RemoveAt(int index) => throw new NotSupportedException();
-
-		/// <summary>
-		/// Gets or sets the element at the specified index.
-		/// </summary>
-		/// <param name="index">The zero-based index of the element to get or set.</param>
-		/// <returns>The element at the specified index.</returns>
-		/// <exception cref="ArgumentOutOfRangeException">
-		/// index is not a valid index.</exception>
-		/// <exception cref="NotSupportedException">
-		/// The property is set and this collection is read-only.</exception>
-		public T this[int index]
-		{
-			get => items[index];
-			set => throw new NotSupportedException();
-		}
+		public TTo this[int index] => items[index];
 	}
 }
