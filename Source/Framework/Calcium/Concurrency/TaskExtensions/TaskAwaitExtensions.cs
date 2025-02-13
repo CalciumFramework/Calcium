9b92e1435f13876a13add4bb05c84129c60b2c3a
Daniel Vaughan <1843788+DVaughan@users.noreply.github.com>
2025-02-13 17:47:08 +0100

    Add TaskAwaitExtensions

---
+++ Source/Framework/Calcium/Concurrency/TaskExtensions/TaskAwaitExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Calcium.Concurrency.TaskExtensions
+{
+	/// <summary>
+	/// Provides extension methods for configuring await behavior on tasks.
+	/// </summary>
+	public static class TaskAwaitExtensions
+	{
+		/// <summary>
+		/// Shorthand for <c>.ConfigureAwait(false)</c>.
+		/// Does not capture the current synchronization context.
+		/// </summary>
+		/// <param name="task">The task to configure.</param>
+		/// <returns>A task that does not capture the current synchronization context.</returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static ConfiguredTaskAwaitable NoSync(this Task task) => task.ConfigureAwait(false);
+
+		/// <summary>
+		/// Shorthand for <c>.ConfigureAwait(false)</c>.
+		/// Does not capture the current synchronization context.
+		/// </summary>
+		/// <typeparam name="T">The return type of the <c>Task</c>.</typeparam>
+		/// <param name="task">The task to configure.</param>
+		/// <returns>A task that does not capture the current synchronization context.</returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static ConfiguredTaskAwaitable<T> NoSync<T>(this Task<T> task) => task.ConfigureAwait(false);
+	}
+}
